97f6e3005c87c4753a0ee4bd68c6e69316c687d3
j1522158 <j1522158vu@gmail.com>
2023-12-31 21:52:15 +0900

    追加

--- src/main/java/com/example/its/domain/issue/IssueRepository.java
+++ src/main/java/com/example/its/domain/issue/IssueRepository.java
@@ -1,6 +1,7 @@
 package com.example.its.domain.issue;
 
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -11,4 +12,10 @@ public interface IssueRepository {
 
     @Select("select * from issues")
     List<IssueEntity> findAll();
+
+    @Insert("insert into issues (summary, description) values (#{summary}, #{description})")
+    void insert(String summary, String description);
+
+    @Select("select * from issues where id = #{issueId}")
+    IssueEntity findById(long issueId);
 }
